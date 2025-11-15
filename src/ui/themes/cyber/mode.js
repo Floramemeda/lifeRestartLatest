0288e1ecec9895c1ba876180473d2955efe64760
Flora Zhou <543155492@qq.com>
2025-11-15 18:19:59 +1300

    remove celebrity mode

--- src/ui/themes/cyber/mode.js
+++ src/ui/themes/cyber/mode.js
@@ -2,6 +2,6 @@ export default class CyberMode extends ui.view.CyberTheme.ModeUI {
     constructor() {
         super();
         this.btnCustom.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.TALENT));
-        this.btnCelebrity.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.CELEBRITY));
+        this.btnCelebrity.removeSelf();
     }
 }
--- src/ui/themes/default/mode.js
+++ src/ui/themes/default/mode.js
@@ -2,6 +2,6 @@ export default class Mode extends ui.view.DefaultTheme.ModeUI {
     constructor() {
         super();
         this.btnCustom.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.TALENT));
-        this.btnCelebrity.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.CELEBRITY));
+        this.btnCelebrity.removeSelf();
     }
 }
