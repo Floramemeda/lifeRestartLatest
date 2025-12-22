e6dbd3f9a025ca7d1156d195eab59ae3ef42d5c1
Flora Zhou <543155492@qq.com>
2025-12-22 16:15:37 +1300

    remove github button

--- src/ui/themes/default/main.js
+++ src/ui/themes/default/main.js
@@ -4,10 +4,7 @@ export default class Main extends ui.view.DefaultTheme.MainUI {
         this.btnRemake.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.MODE));
         this.btnAchievement.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.ACHIEVEMENT));
         this.btnThanks.on(Laya.Event.CLICK, this, ()=>$ui.switchView(UI.pages.THANKS));
-        this.btnGithub.on(Laya.Event.CLICK, this, goto, ['github']);
-        this.btnDiscord.on(Laya.Event.CLICK, this, goto, ['discord']);
         this.btnThemes.on(Laya.Event.CLICK, this, ()=>$ui.showDialog(UI.pages.THEMES));
-        this.btnSaveLoad.on(Laya.Event.CLICK, this, ()=>$ui.showDialog(UI.pages.SAVELOAD));
     }
 
     static load() {
